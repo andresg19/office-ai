3177d35f3ce1164c96f9ea361a681e4de27fd33a
andresg19 <andygermain17@gmail.com>
2023-06-08 23:54:30 -0300

    deleted

--- client/background.js
+++
@@ -1,8 +0,0 @@
-chrome.browserAction.onClicked.addListener(function(tab) {
-    chrome.windows.create({
-      url: chrome.runtime.getURL("./index.html"),
-      type: "normal",
-      width: 800,
-      height: 600
-    });
-  });
