cbe2e63efdf30ecaff99e194dee8fdab4ec45d22
andresg19 <andygermain17@gmail.com>
2023-06-09 04:16:14 -0300

    server updated

--- client/src/Redux/Actions/Actions.js
+++ client/src/Redux/Actions/Actions.js
@@ -5,7 +5,7 @@ import { CORRECT_TEXT, TRANSLATE ,LOADER, CHAT, CLEAR } from "./ActionTypes";
 export const correctText = ( text ) => {
     return async function (dispatch) {
         try {
-            let result = await axios.get(`${process.env.SERVER_URL}/correct?text=${text}`);
+            let result = await axios.get(`https://office-ai-server.vercel.app/correct?text=${text}`);
             return dispatch({
                 type: CORRECT_TEXT,
                 payload: result.data
@@ -23,7 +23,7 @@ export const correctText = ( text ) => {
 export const translateText = ( text, destLang ) => {
     return async function (dispatch) {
         try {
-            let result = await axios.get(`${process.env.SERVER_URL}/translate?text=${text}&destLang=${destLang}`);
+            let result = await axios.get(`https://office-ai-server.vercel.app/translate?text=${text}&destLang=${destLang}`);
             return dispatch({
                 type: TRANSLATE,
                 payload: result.data
@@ -40,7 +40,7 @@ export const translateText = ( text, destLang ) => {
 export const chat = ( text ) => {
     return async function (dispatch) {
         try {
-            let result = await axios.get(`${process.env.SERVER_URL}/chat?text=${text}`);
+            let result = await axios.get(`https://office-ai-server.vercel.app/chat?text=${text}`);
             return dispatch({
                 type: CHAT,
                 payload: result.data
